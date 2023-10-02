4f980b57c2a397271a527343ab07e0156cb9c1d8
it21096570 <it21096570@my.sliit.lk>
2023-10-02 16:02:05 +0530

    Update ENUMs

--- BACK-END/src/entity/master/payment.ts
+++ BACK-END/src/entity/master/payment.ts
@@ -1,5 +1,5 @@
 import { Column, Double, Entity, PrimaryGeneratedColumn, Table } from "typeorm";
-import { Status } from "../../enum/Status";
+import { Status } from "../../enum/status";
 import { uStatus } from "../../enum/uStatus";
 import { mStatus } from "../../enum/mStatus";
 
