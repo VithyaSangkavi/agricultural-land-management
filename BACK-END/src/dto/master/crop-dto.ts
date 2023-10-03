08a4935c61af8667cc6d6c4924df0856f4a59a5d
it21096570 <it21096570@my.sliit.lk>
2023-10-03 09:56:49 +0530

    after 2 Dtos

--- BACK-END/src/dto/master/crop-dto.ts
+++ BACK-END/src/dto/master/crop-dto.ts
@@ -23,6 +23,64 @@ export class CropDto extends PaginationDto {
             this.setStartIndex(body.startIndex);
             this.setMaxResult(body.maxResult);
         }
-    
+
     }
+
+    filViaDbObject(cropModel: CropEntity) {
+        this.crop_id = cropModel.crop_id;
+        this.land_id = cropModel.land_id;
+        this.name = cropModel.name;
+        this.createdDate = cropModel.createdDate;
+        this.updatedDate = cropModel.updatedDate;
+        this.status = cropModel.status;
+    }
+
+    public getCropId(): number {
+        return this.crop_id;
+    }
+
+    public setCropId(crop_id: number): void {
+        this.crop_id = crop_id;
+    }
+
+    public getLandId(): number {
+        return this.land_id;
+    }
+
+    public setLandId(land_id: number): void {
+        this.land_id = land_id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public getCreatedDate(): Date {
+        return this.createdDate;
+    }
+
+    public setCreatedDate(createdDate: Date): void {
+        this.createdDate = createdDate;
+    }
+
+    public getUpdatedDate(): Date {
+        return this.updatedDate;
+    }
+
+    public setUpdatedDate(updatedDate: Date): void {
+        this.updatedDate = updatedDate;
+    }
+
+    public getStatus(): Status {
+        return this.status;
+    }
+
+    public setStatus(status: Status): void {
+        this.status = status;
+    }
+
 }
