a2cf12d10ccb63f733a043eb3ef15ff9f3853c97
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-10-04 09:14:45 +0530

    Service Interfaces

---
+++ BACK-END/src/services/master/worker-service.ts
@@ -0,0 +1,10 @@
+import { CommonResponse } from "../../common/dto/common-response";
+import { WorkerDto } from "../../dto/master/worker-dto";
+
+export interface WorkerService {
+  save(workerDto: WorkerDto): Promise<CommonResponse>;
+  update(workerDto: WorkerDto): Promise<CommonResponse>;
+  delete(workerDto: WorkerDto): Promise<CommonResponse>;
+  find(workerDto: WorkerDto): Promise<CommonResponse>;
+  findById(workerId: number): Promise<CommonResponse>;
+}
