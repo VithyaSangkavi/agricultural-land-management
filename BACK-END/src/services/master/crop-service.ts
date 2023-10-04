71247eb8c0074b5bc0b09590bd67bd8850165318
it21096570 <it21096570@my.sliit.lk>
2023-10-04 09:21:49 +0530

    after complete services

---
+++ BACK-END/src/services/master/crop-service.ts
@@ -0,0 +1,10 @@
+import { CommonResponse } from "../../common/dto/common-response";
+import { CropDto } from "../../dto/master/crop-dto";
+
+export interface CropService {
+    save(cropDto: CropDto): Promise<CommonResponse>;
+    update(cropDto: CropDto): Promise<CommonResponse>;
+    delete(cropDto: CropDto): Promise<CommonResponse>;
+    findAll(cropDto: CropDto): Promise<CommonResponse[]>;
+    findById(crop_id: number): Promise<CommonResponse>;
+}
