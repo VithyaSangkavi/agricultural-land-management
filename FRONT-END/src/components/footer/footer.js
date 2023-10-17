93dba8291afefbebd7a4045ad971f51cee2ba9e8
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-10-17 16:11:41 +0530

    Footer

--- FRONT-END/src/components/footer/footer.js
+++ FRONT-END/src/components/footer/footer.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faHome, faChartPie } from '@fortawesome/free-solid-svg-icons';
 import './footer.css'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar';
 
 const Footer = () => {
     const [isSideNavOpen, setIsSideNavOpen] = useState(false);
@@ -10,20 +11,23 @@ const Footer = () => {
 
     const toggleSideNav = () => {
         setIsSideNavOpen(!isSideNavOpen);
-        history.push('/sideNavBar')
+        history.push('/sideNavPage')
     };
 
     return (
-        <div className='footer-app-screen'>
-            <div className="footer-container">
-                <div className="footer-icon">
-                    <FontAwesomeIcon icon={faBars} size="2x" onClick={toggleSideNav} />
-                </div>
-                <div className="footer-icon">
-                    <FontAwesomeIcon icon={faHome} size="2x" />
-                </div>
-                <div className="footer-icon">
-                    <FontAwesomeIcon icon={faChartPie} size="2x" />
+        <div className='app-screen'>
+            <div className='footer-app-screen'>
+                <div className="footer-container">
+
+                    <div className="footer-icon">
+                        <FontAwesomeIcon icon={faBars} size="2x" onClick={toggleSideNav} />
+                    </div>
+                    <div className="footer-icon">
+                        <FontAwesomeIcon icon={faHome} size="2x" />
+                    </div>
+                    <div className="footer-icon">
+                        <FontAwesomeIcon icon={faChartPie} size="2x" />
+                    </div>
                 </div>
             </div>
         </div>
