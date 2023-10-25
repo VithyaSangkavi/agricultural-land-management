646b52d18638ad5148b605d1b98216949dde48bc
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-10-25 10:08:25 +0530

    Did some modifications

--- FRONT-END/src/components/home/home.js
+++ FRONT-END/src/components/home/home.js
@@ -11,15 +11,23 @@ function Home() {
     const [selectedLand, setSelectedLand] = useState('');
 
     const [searchQuery, setSearchQuery] = useState('');
+
+    const [query, setQuery] = useState('');
     const [task, setTask] = useState([]);
+    const [taskNames, setTaskNames] = useState([]);
+    const [taskAssigned, setTaskAssigned] = useState([]);
 
     const history = useHistory();
 
     useEffect(() => {
+        axios.post('http://localhost:8081/service/master/taskAssignedFindAll').then((response) => {
+            setTaskAssigned(response.data);
+            console.log("Task Assigned: ", response.data);
+        });
 
         axios.post('http://localhost:8081/service/master/taskFindAll').then((response) => {
             setTask(response.data.extra);
-            console.log("Expenses : ", response.data.extra);
+            console.log("Tasks : ", response.data.extra);
         });
 
         axios.get('http://localhost:8081/service/master/landFindAll').then((response) => {
@@ -41,10 +49,35 @@ function Home() {
         history.push(`/addTask`);
     };
 
+    const handleSelectedLand = (eventkey) => {
+        setSelectedLand(eventkey);
+
+        axios.post(`http://localhost:8081/service/master/findLandIdByName?name=${eventkey}`)
+            .then((response) => {
+                const landIdTask = response.data.extra;
+                const taskLand = JSON.stringify(landIdTask);
+                const landData = JSON.parse(taskLand);
+                const landId = landData.landId;
+                console.log('Selected Land Id :', landId);
+                localStorage.setItem('SelectedLandId', landId);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }
+
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    };
+    const filteredTaskAssigned = Array.isArray(taskAssigned)
+        ? taskAssigned.filter((task) => task.taskAssignedId)
+        : [];
+
+
     return (
         <div className="home-app-screen">
             <div className='drop-down-container'>
-                <Dropdown className='custom-dropdown'>
+                <Dropdown onSelect={handleSelectedLand} className='custom-dropdown'>
                     <Dropdown.Toggle className='drop-down' id="dropdown-land">
                         {selectedLand || 'Select Land'}
                     </Dropdown.Toggle>
@@ -57,6 +90,16 @@ function Home() {
                     </Dropdown.Menu>
                 </Dropdown>
                 <br />
+            </div>
+            <div className='home-heading'>
+                <p>Ongoing Tasks</p>
+            </div>
+            <div className="task-list">
+                {filteredTaskAssigned.map((taskAssigned) => (
+                    <div key={taskAssigned.id} className="task-card" onClick={() => handleCardClick(taskAssigned.id)}>
+                        <p>{taskAssigned.taskName}</p>
+                    </div>
+                ))}
             </div>
             <div className='home-heading'>
                 <p>New Task</p>
