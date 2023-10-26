252c95408c39a26ecfd25d5cb9b086693d9d1a3c
it21096570 <it21096570@my.sliit.lk>
2023-10-26 14:33:41 +0530

    lamguage change done for income part

--- FRONT-END/src/components/lot/insert_lot.js
+++ FRONT-END/src/components/lot/insert_lot.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom'; // Import useLocation
 import axios from 'axios';
 import '../lot/insert_lot.css';
@@ -8,18 +8,24 @@ import { submitCollection } from '../../_services/submit.service';
 import { Form, Button, Container, Col, Row, Card } from 'react-bootstrap';
 import { submitSets } from '../UiComponents/SubmitSets';
 import { alertService } from '../../_services/alert.service';
+import { useTranslation } from 'react-i18next';
+
+
 
 const InsertLot = () => {
-
     const [name, setName] = useState('');
     const [area, setArea] = useState('');
     const [areaUom, setAreaUom] = useState('');
     const [selectedLandId, setSelectedLandId] = useState('1');
-    const [selectedLanguage, setSelectedLanguage] = useState('english');
+    const [selectedLanguage, setSelectedLanguage] = useState('en');
 
-    /* const location = useLocation();
-    const landId = location.state?.landId;  // Grab landId from state data
-    console.log(landId); */
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        console.log("Trying to change language to:", selectedLanguage);
+        i18n.changeLanguage(selectedLanguage);
+        console.log("Language should be changed now.");
+    }, [selectedLanguage]);
 
     const handleSubmit = () => {
 
@@ -58,40 +64,40 @@ const InsertLot = () => {
                     <Row className="justify-content-center">
                         <Col sm={6}>
                             <Card className="card-container">
-                                <Card.Header className="card-title">Add Lot</Card.Header>
+                                <Card.Header className="card-title">{t('addlots')}</Card.Header>
                                 <Card.Body>
                                     <Form>
                                         <Form.Group controlId="name">
-                                            <Form.Label className="form-label">Name</Form.Label>
+                                            <Form.Label className="form-label">{t('name')}</Form.Label>
                                             <Form.Control
                                                 className="input-field"
                                                 type="text"
-                                                placeholder="Lot Name"
+                                                placeholder={`${t('lot')} ${t('name')}`}
                                                 value={name}
                                                 onChange={(e) => setName(e.target.value)}
                                             />
                                         </Form.Group>
                                         <Form.Group controlId="area">
-                                            <Form.Label className="form-label">Area</Form.Label>
+                                            <Form.Label className="form-label">{t('area')}</Form.Label>
                                             <Form.Control
                                                 className="input-field"
                                                 type="text"
-                                                placeholder="Lot Area"
+                                                placeholder={t('area')}
                                                 value={area}
                                                 onChange={(e) => setArea(e.target.value)}
                                             />
                                         </Form.Group>
                                         <Form.Group controlId="areaUom">
-                                            <Form.Label className="form-label">Area UOM</Form.Label>
+                                            <Form.Label className="form-label">{t('area')} UOM</Form.Label>
                                             <Form.Control
                                                 className="input-field"
                                                 as="select"
                                                 value={areaUom}
                                                 onChange={(e) => setAreaUom(e.target.value)}
                                             >
-                                                <option value="">Select Area UOM</option>
-                                                <option value="arce">Arce</option>
-                                                <option value="perch">Perch</option>
+                                                <option value="">{t('select')}</option>
+                                                <option value="arce">{t('arce')}</option>
+                                                <option value="perch">{t('perch')}</option>
                                             </Form.Control>
                                         </Form.Group>
                                         <Button
@@ -99,7 +105,7 @@ const InsertLot = () => {
                                             variant="primary"
                                             onClick={handleSubmit}
                                         >
-                                            ADD
+                                            {t('add')}
                                         </Button>
                                     </Form>
                                 </Card.Body>
