d5644088a5c293aec3c489184cbfff3fb27e2d66
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-10-31 17:27:29 +0530

    Added a small animation for side nav menu bar

--- FRONT-END/src/components/sideNav/SideNavPage.js
+++ FRONT-END/src/components/sideNav/SideNavPage.js
@@ -6,15 +6,12 @@ import { useHistory } from 'react-router-dom';
 import { FaGlobeAmericas, FaLanguage } from 'react-icons/fa';
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import AnimatedPage from '../../AnimatedPage';
 
 function SideNavBar() {
 
-
   const history = useHistory();
   const { t, i18n } = useTranslation();
-
-
-
 
   const ManageWorkers = () => {
     history.push('/manageWorkers');
@@ -45,72 +42,75 @@ function SideNavBar() {
   };
 
   return (
-    <div className='side-nav-screen'>
-      <p className='main-heading'>{t('welcome')}</p>
-      <div className="position-absolute top-0 end-0 mt-2 me-2">
-        <Dropdown alignRight onSelect={handleLanguageChange}>
-          <Dropdown.Toggle variant="secondary" style={{ background: 'none', border: 'none' }}>
-            <FaGlobeAmericas style={{ color: 'white' }} />
-          </Dropdown.Toggle>
+    <AnimatedPage>
+      <div className='side-nav-screen'>
+        <p className='main-heading'>{t('welcome')}</p>
+        <div className="position-absolute top-0 end-0 mt-2 me-2">
+          <Dropdown alignRight onSelect={handleLanguageChange}>
+            <Dropdown.Toggle variant="secondary" style={{ background: 'none', border: 'none' }}>
+              <FaGlobeAmericas style={{ color: 'white' }} />
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item eventKey="en">English</Dropdown.Item>
-            <Dropdown.Item eventKey="sl">Sinhala</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-      <Navbar className="side-nav-menu">
-        <Container>
-          <Navbar.Brand onClick={ManageWorkers}>
-            {t('workermanagement')}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="side-nav-menu">
-        <Container>
-          <Navbar.Brand onClick={ManageLand}>
-            {t('managelands')}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="side-nav-menu">
-        <Container>
-          <Navbar.Brand onClick={ManageLot}>
-            {t('managelots')}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="side-nav-menu">
-        <Container>
-          <Navbar.Brand onClick={ManageTaskType}>
-            {t('managetasktype')}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="side-nav-menu">
-        <Container>
-          <Navbar.Brand onClick={ManageIncome}>
-            {t('manageincome')}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <Navbar className="side-nav-menu">
-        <Container>
-          <Navbar.Brand onClick={ManageExpenseType}>
-            {t('manageexpensetype')}
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <br />
-      <div className='footer-alignment'>
-        <Footer />
-      </div>
-    </div >
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="en">English</Dropdown.Item>
+              <Dropdown.Item eventKey="sl">Sinhala</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <Navbar className="side-nav-menu">
+          <Container>
+            <Navbar.Brand onClick={ManageWorkers}>
+              {t('workermanagement')}
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar className="side-nav-menu">
+          <Container>
+            <Navbar.Brand onClick={ManageLand}>
+              {t('managelands')}
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar className="side-nav-menu">
+          <Container>
+            <Navbar.Brand onClick={ManageLot}>
+              {t('managelots')}
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar className="side-nav-menu">
+          <Container>
+            <Navbar.Brand onClick={ManageTaskType}>
+              {t('managetasktype')}
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar className="side-nav-menu">
+          <Container>
+            <Navbar.Brand onClick={ManageIncome}>
+              {t('manageincome')}
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+        <br />
+        <Navbar className="side-nav-menu">
+          <Container>
+            <Navbar.Brand onClick={ManageExpenseType}>
+              {t('manageexpensetype')}
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        <br />
+        <div className='footer-alignment'>
+          <Footer />
+        </div>
+      </div >
+    </AnimatedPage >
   );
 }
 
