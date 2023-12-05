742305bf359dd5adb9e97e634f2cd7e84b78e4b8
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-12-05 14:39:02 +0530

    Report Backend (seperate service and DAO classes)

--- BACK-END/src/services/master/reports-service.ts
+++ BACK-END/src/services/master/reports-service.ts
@@ -1,6 +1,5 @@
   export interface ReportService {
-    generateEmployeeAttendanceReport(startDate?: Date, endDate?: Date, lotId?: number): Promise<any>;
-    generateMonthlyCropReport(lotId?: number, startDate?: Date, endDate?: Date): Promise<any>;
-    generateOtherCostYieldReport(): Promise<any>;
+    generateEmployeeAttendanceReport(startDate: Date, endDate: Date, lotId: number): Promise<any>;
+    generateMonthlyCropReport(lotId: number, startDate: Date, endDate: Date): Promise<any>;
+    generateOtherCostYieldReport(startDate: Date, endDate: Date): Promise<any>;
   }
-  
