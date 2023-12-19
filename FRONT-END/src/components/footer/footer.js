151c299d1ebbeb6e5fb86b2155592d51ddbc3a47
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-12-19 12:18:18 +0530

    Footer icon hightlight on select

--- FRONT-END/src/components/footer/footer.js
+++ FRONT-END/src/components/footer/footer.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faHome, faChartPie } from '@fortawesome/free-solid-svg-icons';
 import './footer.css'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar';
 import axios from 'axios';
 
 
 const Footer = () => {
     const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+    const [activePage, setActivePage] = useState('');
     const history = useHistory();
+    const location = useLocation();
+
+    useEffect(() => {
+        setActivePage(location.pathname);
+    }, [location]);
 
     const toggleSideNav = () => {
         setIsSideNavOpen(!isSideNavOpen);
@@ -28,18 +34,22 @@ const Footer = () => {
             })
     }
 
+    const getIconColor = (iconName) => {
+        return activePage === `/${iconName}` ? 'active-icon' : 'inactive-icon';
+    };
+
     return (
         <div className='app-screen'>
             <div className='footer-app-screen'>
                 <div className="footer-container">
 
-                    <div className="footer-icon">
+                    <div className={`footer-icon ${getIconColor('sideNavPage')}`}>
                         <FontAwesomeIcon icon={faBars} size="2x" onClick={toggleSideNav} />
                     </div>
-                    <div className="footer-icon">
+                    <div className={`footer-icon ${getIconColor('home')}`}>
                         <FontAwesomeIcon icon={faHome} size="2x" onClick={clickHome} />
                     </div>
-                    <div className="footer-icon">
+                    <div className={`footer-icon ${getIconColor('report')}`}>
                         <FontAwesomeIcon icon={faChartPie} size="2x" onClick={clickReport} />
                     </div>
                 </div>
