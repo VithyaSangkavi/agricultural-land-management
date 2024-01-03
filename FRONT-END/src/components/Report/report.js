28c0d91fe17b459402eab16e7620fb20a5f5602d
VithyaSangkavi <vithyasangkavi@gmail.com>
2024-01-03 15:49:57 +0530

    Added redux state to store the crop name for selected land

--- FRONT-END/src/components/Report/report.js
+++ FRONT-END/src/components/Report/report.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import '../home/home.css';
 import './report.css'
 import Footer from '../footer/footer';
+import Header from '../header/header'
 import { FaGlobeAmericas, FaMapMarker } from 'react-icons/fa';
 import { Dropdown } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
@@ -233,47 +234,7 @@ function Report({ setSelectedLandId, selectedLandId }) {
 
     return (
         <div className="home-app-screen">
-            <div className='main-heading'>
-
-                <div className="outer-frame d-flex justify-content-between align-items-center">
-                    <div className="filter-container d-flex align-items-center">
-                        <MdArrowBackIos className="back-button" onClick={goBack} />
-                    </div>
-
-                    <div className="filter-container d-flex align-items-center">
-                        <div className="land-filter">
-                            <Dropdown onSelect={handleLandChange}>
-                                <Dropdown.Toggle variant="secondary" style={{ background: 'none', border: 'none' }}>
-                                    <FaMapMarker style={{ color: 'white' }} />
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item eventKey="">All Lands</Dropdown.Item>
-                                    {landNames.map((land) => (
-                                        <Dropdown.Item eventKey={land.id} value={land.id}>
-                                            {land.name}
-                                        </Dropdown.Item>
-                                    ))}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
-
-                        <div className="language-filter">
-                            <Dropdown onSelect={handleLanguageChange}>
-                                <Dropdown.Toggle variant="secondary" style={{ background: 'none', border: 'none' }}>
-                                    <FaGlobeAmericas style={{ color: 'white' }} />
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item eventKey="en">English</Dropdown.Item>
-                                    <Dropdown.Item eventKey="sl">Sinhala</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <Header/>
             <div className="drop-down-container" style={{ marginTop: "0px" }}>
 
                 <div className='landsectioncover'>
